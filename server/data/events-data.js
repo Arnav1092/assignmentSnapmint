40cc25dcc9b5b61f787de763f10bacf52bcc37f1
Arnav1092 <65768617+Arnav1092@users.noreply.github.com>
2023-03-16 17:00:56 +0530

    Add files via upload

---
+++ server/data/events-data.js
@@ -0,0 +1,111 @@
+var events = [{
+    "id": "1",
+    "topics": "sports",
+    "thumbnail": "",
+    "url": "index.html",
+    "overrideURL": "",
+    "linkType": "",
+    "title": "College Basketball: Jaipur Cllege",
+    "summary": "Jaipur capital of Rajasthan."
+}, {
+    "id": "2",
+    "topics": "",
+    "thumbnail": "",
+    "url": "index.html",
+    "overrideURL": "",
+    "linkType": "",
+    "title": "IBM",
+    "summary": "Hear from executives and clients who will share their insights and experiences. Learn how to capitalize on the disruptive, fast-moving transformations like cloud, mobile, cognitive and analytics that are changing how we connect and collaborate."
+}, {
+    "id": "3",
+    "topics": "business",
+    "thumbnail": "",
+    "url": "index.html",
+    "overrideURL": "",
+    "linkType": "",
+    "title": "WIRED Business Conference",
+    "summary": "Business Conference"
+}, {
+    "id": "4",
+    "topics": "sports",
+    "thumbnail": "",
+    "url": "index.html",
+    "overrideURL": "",
+    "linkType": "",
+    "title": "BASE Jumping",
+    "summary": "is very extreme activity that includes a parachute (can be used both parachute and wingsuit) to jump from fixed objects, with unopened parachute like skydiving. The acronym “B.A.S.E.” stands for: Building, Antenna, Span, Earth – four categories of objects from which B.A.S.E. jumper can jump."
+}, {
+    "id": "5",
+    "topics": "",
+    "thumbnail": "",
+    "url": "index.html",
+    "overrideURL": "",
+    "linkType": "",
+    "title": "Transport 1",
+    "summary": "Lorem ipsum dolor sit amet"
+}, {
+    "id": "6",
+    "topics": "sports",
+    "thumbnail": "",
+    "url": "index.html",
+    "overrideURL": "",
+    "linkType": "",
+    "title": "Olympic Trials",
+    "summary": "Trials For Olympics."
+}, {
+    "id": "7",
+    "topics": "business",
+    "thumbnail": "",
+    "url": "index.html",
+    "overrideURL": "",
+    "linkType": "",
+    "title": "Business 3",
+    "summary": "Lorem ipsum dolor sit amet"
+}, {
+    "id": "8",
+    "topics": "technics",
+    "thumbnail": "",
+    "url": "index.html",
+    "overrideURL": "",
+    "linkType": "",
+    "title": "Tech 1",
+    "summary": "Ut enim ad minim veniam"
+}, {
+    "id": "9",
+    "topics": "transport",
+    "thumbnail": "",
+    "url": "index.html",
+    "overrideURL": "",
+    "linkType": "",
+    "title": "Autonomous Cars",
+    "summary": "Lorem ipsum dolor sit amet"
+}, {
+    "id": "10",
+    "topics": "technics",
+    "thumbnail": "",
+    "url": "index.html",
+    "overrideURL": "",
+    "linkType": "",
+    "title": "Tech 2",
+    "summary": "Lorem ipsum dolor sit amet"
+}, {
+    "id": "11",
+    "topics": "fashion",
+    "thumbnail": "",
+    "url": "index.html",
+    "overrideURL": "",
+    "linkType": "",
+    "title": "Fashion Week",
+    "summary": "Fashion Week is coming 'round the bend!"
+}, {
+    "id": "12",
+    "topics": "transport",
+    "thumbnail": "",
+    "url": "index.html",
+    "overrideURL": "",
+    "linkType": "",
+    "title": "Transport 3",
+    "summary": "Lorem ipsum dolor sit amet"
+}];
+
+module.exports = events;
